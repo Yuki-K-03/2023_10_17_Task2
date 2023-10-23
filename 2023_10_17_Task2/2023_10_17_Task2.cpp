7e206e2b9fa93d6166e966560eccacb93e91bbf1
Owner PC <PC-Owner-t@outlook.jp>
2023-10-23 23:50:49 +0900

    プロジェクト ファイルを追加します。

---
+++ 2023_10_17_Task2/2023_10_17_Task2.cpp
@@ -0,0 +1,83 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <cstring>
+using namespace std;
+
+
+class Hero {
+public :
+    Hero(int size);
+    ~Hero();
+
+public:
+    int Get(int i);
+    void Set(int i, int value);
+
+private:
+    // インデックスのチェック
+    void CheckIndex(int i);
+
+private :
+    int* name_array;
+    int name_size;
+
+private :
+    int hp;
+    int statusAttack;
+    int statusDiffence;
+};
+
+// コンストラクタ
+Hero::Hero(int size) {
+    name_array = new int[size];
+    name_size = size;
+}
+
+// デストラクタ
+Hero::~Hero() {
+    delete[] name_array;
+}
+
+
+
+class Enemy {
+public:
+    Enemy(int size);
+    ~Enemy();
+
+public:
+    int Get(int i);
+    void Set(int i, int value);
+
+private:
+    // インデックスのチェック
+    void CheckIndex(int i);
+
+private:
+    int* name_array;
+    int name_size;
+
+private:
+    int hp;
+    int statusAttack;
+    int statusDiffence;
+};
+
+void InputStatus() {
+
+}
+
+void attack() {
+
+}
+
+void heal() {
+
+}
+
+int main()
+{
+    while () {
+
+    }
+}
